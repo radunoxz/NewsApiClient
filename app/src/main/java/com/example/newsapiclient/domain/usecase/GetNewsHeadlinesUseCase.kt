a248ca3392f3193482cedcaf290b3945d4d4c1d0
Radu Cosac <cosar30@yahoo.com>
2024-12-10 21:06:48 +0100

    Added architeture layers + defined usecases

---
+++ app/src/main/java/com/example/newsapiclient/domain/usecase/GetNewsHeadlinesUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.newsapiclient.domain.usecase
+
+import com.example.newsapiclient.domain.repository.NewsRepository
+
+class GetNewsHeadlinesUseCase(
+    private val newsRepository: NewsRepository
+) {
+}
